ea60db934b1c1731ed82bf6db641ed99f41207d2
4RCZ1 <marcinowicz1@gmail.com>
2025-08-25 18:34:09 +0200

    feat: add basic binance api functions

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
 
 import type MessageResponse from "../interfaces/message-response.js";
+import type { Kline } from "../services/binance-api.js";
 
-import { getMarketData, okTest } from "../services/binance-api.js";
+import { getKlines, getMarketData, okTest } from "../services/binance-api.js";
 import emojis from "./emojis.js";
 
 const router = express.Router();
@@ -17,6 +18,12 @@ router.get<object, Response>("/market-data", async (req, res) => {
   const marketData = await getMarketData("BTCUSDT");
   console.warn("marketData", marketData);
   res.json(marketData);
+});
+
+router.get<object, Kline[]>("/klines", async (req, res) => {
+  const marketData = await getKlines("BTCUSDT");
+  console.warn("marketData", marketData);
+  res.send(marketData);
 });
 
 router.use("/emojis", emojis);
--- src/services/binance-api.ts
+++ src/services/binance-api.ts
@@ -9,3 +9,29 @@ export async function getMarketData(symbol: string) {
   path.searchParams.set("symbol", symbol);
   return fetch(path, {});
 }
+
+export type Kline = {
+  openTime: number;
+  openPrice: number;
+  highPrice: number;
+  lowPrice: number;
+  closePrice: number;
+  closeTime: number;
+};
+
+export async function getKlines(symbol: string, interval: string = "1d"): Promise<Kline[]> {
+  const path = new URL(`${BASE_URL}/api/v3/klines`);
+  path.searchParams.set("symbol", symbol);
+  path.searchParams.set("interval", interval);
+  const result = await (await fetch(path, {})).json();
+  return result.map((rawObject: any): Kline => (
+    {
+      openTime: rawObject[0],
+      openPrice: rawObject[1],
+      highPrice: rawObject[2],
+      lowPrice: rawObject[3],
+      closePrice: rawObject[4],
+      closeTime: rawObject[6],
+    }
+  ));
+}
